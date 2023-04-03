d4b1f7aaa64bfd15778f6b424efd9d149746519e
BrandusaMoahnu <brandusamohanu@gmail.com>
2023-04-03 16:52:56 +0200

    js

---
+++ m1/s2/g1/es1/js/js1.js
@@ -0,0 +1,15 @@
+var colorepreferito = 'verde';
+var $colore ='blu';
+console.log($colore);
+var _colore ='giallo';
+console.log(_colore);
+var _colore2 = 'rosa';
+console.log(_colore2)
+document.write($colore);
+var numero = 5;
+var $nr= '10'
+console.log($nr);
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
