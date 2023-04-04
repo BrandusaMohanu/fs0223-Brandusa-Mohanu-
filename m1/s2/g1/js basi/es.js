966e57e0be94981a19cc315c6dd2de9d8363e7b4
BrandusaMoahnu <brandusamohanu@gmail.com>
2023-04-04 15:28:08 +0200

    es2

---
+++ m1/s2/g1/js basi/es.js
@@ -0,0 +1,10 @@
+function accendi(){
+    var lampadina = document.getElementById('bulb');
+   
+    lampadina.style.backgroundColor = 'yellow';
+}
+function spegni(){
+    var lampadina = document.getElementById('bulb');
+   
+    lampadina.style.backgroundColor = 'black';
+}
