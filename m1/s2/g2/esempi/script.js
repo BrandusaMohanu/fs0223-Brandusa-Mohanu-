91a16359bb0e00267f1689ac3ed32808ef8d57a3
BrandusaMoahnu <brandusamohanu@gmail.com>
2023-04-05 17:09:28 +0200

    js

---
+++ m1/s2/g2/esempi/script.js
@@ -0,0 +1,12 @@
+//function addNumber(elemento)
+function addNumber(n){
+   // console.log(elemento);
+    let display = document.getElementById('display');
+   // console.log(display.valute)
+   display.valute += n;
+
+}
+//elemento sta ricevendo this di html
+//elemento puo assumere qualsisi nome
+//addnumber assume cio che gli indichiamo in html nelle paranetesi ()
+ 
